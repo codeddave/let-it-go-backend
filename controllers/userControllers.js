93e887f345b314ca1f69d507f682d26ddb38b0b7
codeddave <drizzzydave@gmail.com>
2021-11-16 20:11:20 +0100

    setting up user routes and controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,6 @@
+const signIn = async (req, res, next) => {};
+
+const signUp = async (req, res, next) => {};
+
+exports.signIn = signIn;
+exports.signUp = signUp;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const userControllers = require("../controllers/userControllers");
 
 const router = express.Router();
+
+router.post("/signup", userControllers.signUp);
+router.post("/signIn", userControllers.signIn);
 
 module.exports = router;
