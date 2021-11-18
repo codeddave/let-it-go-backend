85d83a313e5b31f86b0955203dd559844ee7142b
codeddave <drizzzydave@gmail.com>
2021-11-18 15:59:09 +0100

    built sign in controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -26,7 +26,31 @@ const signUp = async (req, res, next) => {
   }
 };
 
-const signIn = async (req, res, next) => {};
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return next(new HttpError("please provide email and password", 400));
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) return next(new HttpError("user does not exist", 404));
+
+    const passwordsMatch = await existingUser.matchPasswords(password);
+    if (!passwordsMatch) return next(new HttpError("Invalid credentials", 400));
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1hr" }
+    );
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return next(new HttpError("Sign In failed, please try again later", 500));
+  }
+};
 
 exports.signIn = signIn;
 exports.signUp = signUp;
