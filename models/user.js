a371db393a8fa18962093c73580c8e59ecc230e0
codeddave <drizzzydave@gmail.com>
2021-11-18 14:26:53 +0100

    created instance methods on the user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcyrpt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
@@ -13,5 +14,18 @@ const UserSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcyrpt.genSalt(10);
+  this.password = await bcyrpt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.matchPasswords = async function (password) {
+  return await bcyrpt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
