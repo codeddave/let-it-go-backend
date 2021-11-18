27d9960ebf6f092d1e9c7f23985be8f2425ca63a
codeddave <drizzzydave@gmail.com>
2021-11-18 08:47:53 +0100

    updates

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, trim: true },
   email: {
     type: String,
     required: [true, "Please provide an email"],
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,6 @@ const userControllers = require("../controllers/userControllers");
 const router = express.Router();
 
 router.post("/signup", userControllers.signUp);
-router.post("/signIn", userControllers.signIn);
+router.post("/signin", userControllers.signIn);
 
 module.exports = router;
