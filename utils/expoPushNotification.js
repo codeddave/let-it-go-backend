1bfa2c2aae8734ee93ced1c17cff2accee3f78bc
codeddave <drizzzydave@gmail.com>
2021-12-08 12:15:50 +0100

    updates to expoPushNotification function

--- utils/expoPushNotification.js
+++ utils/expoPushNotification.js
@@ -1,9 +1,16 @@
 const { Expo } = require("expo-server-sdk");
 
-const sendPushnotification = (message, expoPushToken) => {
+const sendPushnotification = async (message, expoPushToken) => {
   if (!Expo.isExpoPushToken(expoPushToken)) {
     console.error(`Push token ${pushToken} is not a valid Expo push token`);
   }
+
+  const expo = new Expo();
+  const chunks = expo.chunkPushNotifications([
+    { to: expoPushToken, sound: "default", body: message },
+  ]);
+
+  const sendChunks = async () => {};
 };
 
 module.exports = sendPushnotification;
