b52975a4a294e6470fbed823bb3b554a7cf4cf39
codeddave <drizzzydave@gmail.com>
2021-12-08 21:50:56 +0100

    updated to the expoPushNotification util function

--- utils/expoPushNotification.js
+++ utils/expoPushNotification.js
@@ -9,8 +9,52 @@ const sendPushnotification = async (message, expoPushToken) => {
   const chunks = expo.chunkPushNotifications([
     { to: expoPushToken, sound: "default", body: message },
   ]);
+  let tickets = [];
 
-  const sendChunks = async () => {};
+  const sendChunks = async () => {
+    chunks.forEach(async (chunk) => {
+      console.log("Sending chunk ", chunk);
+
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        tickets.push(...ticketChunk);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+  sendChunks();
+  let receiptIds = [];
+  for (let ticket of tickets) {
+    if (ticket.id) {
+      receiptIds.push(ticket.id);
+    }
+  }
+
+  let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+  (async () => {
+    for (let chunk of receiptIdChunks) {
+      try {
+        let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+        console.log(receipts);
+        for (let receiptId in receipts) {
+          let { status, message, details } = receipts[receiptId];
+          if (status === "ok") {
+            continue;
+          } else if (status === "error") {
+            console.error(
+              `There was an error sending a notification: ${message}`
+            );
+            if (details && details.error) {
+              console.error(`The error code is ${details.error}`);
+            }
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
 };
 
 module.exports = sendPushnotification;
