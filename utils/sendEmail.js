e8d7a942e1697566ab830d7d4ef20608580ebcbe
codeddave <drizzzydave@gmail.com>
2021-12-10 21:11:00 +0100

    updated to sendEmail util file

---
+++ utils/sendEmail.js
@@ -0,0 +1,11 @@
+const nodemailer = require("nodemailer");
+
+const senEmail = (options) => {
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+};
